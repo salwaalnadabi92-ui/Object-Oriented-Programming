91e78521f7cc23f6021513513efcbf481888df18
salwaalnadabi92-ui <salwaalnadabi92@gmail.com>
2026-06-16 08:45:49 +0400

    add main menu and class

--- OOP_Part1/Program.cs
+++ OOP_Part1/Program.cs
@@ -1,10 +1,264 @@
 ﻿namespace OOP_Part1
 {
-    internal class Program
+    class Room
     {
+        int roomNumber;
+        string roomType;
+        double pricePerNight;
+        bool   isAvailable;
+
+
+    }
+
+    class Guest
+    {
+            int     guestId;
+            string  guestName;
+            int     roomNumber;
+            DateTime checkInDate;
+            int      totalNights;
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    internal class Program
+
+    {
+        public static void displayRoom()
+        {
+
+        }
+
+        public static void displayGuest()
+        {
+
+        }
+        public static void  calculateTotalCost()
+
+        {
+
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            List<Room> rooms = new List<Room>();
+
+
+            List<Guest> guests = new List<Guest>();
+
+
+
+
+
+
+            bool exit = false;
+
+            while (exit == false)
+            {
+
+                Console.WriteLine(" =======================================");
+                Console.WriteLine("GRAND VISTA HOTEL — MANAGEMENT SYSTEM ");
+                Console.WriteLine(" =======================================");
+                Console.WriteLine("1.Add New Room ");
+                Console.WriteLine("2.Register New Guest ");
+                Console.WriteLine("3.Book a Room for a Guest");
+                Console.WriteLine("4.Search & Filter Rooms ");
+                Console.WriteLine("5.Guest & Booking Statistics. ");
+                Console.WriteLine("6.Check Out a Guest");
+                Console.WriteLine("7.Remove Unavailable Rooms ");
+                Console.WriteLine("0.Exit");
+                Console.WriteLine("========================================");
+                Console.WriteLine("  Enter your choose");
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+
+                    case 1:
+
+                       
+
+                        break;
+
+                    case 2:
+                    
+
+                        break;
+
+                    case 3:
+                      
+
+                        break;
+
+                    case 4:
+
+                     
+
+                        break;
+
+                    case 5:
+
+
+                        break;
+
+                    case 6:
+
+
+
+                        break;
+
+                    case 7:
+
+
+                    default:
+
+                        break;
+
+                }//switch
+
+
+                Console.WriteLine("Enter any key");
+
+                Console.ReadKey();
+                Console.Clear();
+
+            }//while
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
